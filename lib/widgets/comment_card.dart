637ad47c11b221e30d9e46560d7c51152b96a5eb
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2022-09-13 17:12:59 +0200

    add system of like

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../models/user.dart';
+import '../providers/user_provider.dart';
+import '../ressources/firestone_methods.dart';
+import 'like_animation.dart';
 
 class CommentCard extends StatefulWidget {
   final snap;
@@ -13,6 +19,10 @@ class _CommentCardState extends State<CommentCard> {
 
   @override
   Widget build(BuildContext context) {
+
+    final User user = Provider
+        .of<UserProvider>(context)
+        .getUser;
 
     return Container(
       padding: const EdgeInsets.symmetric(
@@ -59,25 +69,55 @@ class _CommentCardState extends State<CommentCard> {
                     padding: const EdgeInsets.only(
                       top: 4,
                     ),
-                    child: Text(
-                      DateFormat.yMMMd().format(widget.snap['datePublished'].toDate(),),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                      ),
+                    child: Row(
+                      children: [
+                        Text(
+                          DateFormat.yMMMd().format(widget.snap['datePublished'].toDate(),),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        if(widget.snap['likes'].length > 0)
+                        Text(
+                            '${widget.snap['likes'].length} likes',
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Icon(
-              Icons.favorite,
-              size: 16,
+          LikeAnimation(
+            isAnimating: widget.snap['likes'].contains(user.uid),
+            smallLike: true,
+            child: IconButton(
+              onPressed: () async {
+                await FirestoreMethods().likeComment(
+                  widget.snap['postId'],
+                  widget.snap['commentId'],
+                  user.uid,
+                  widget.snap['likes'],
+                );
+              },
+              icon: widget.snap['likes'].contains(user.uid)
+                  ? const Icon(
+                Icons.favorite,
+                color: Colors.red,
+              )
+                  : const Icon(
+                Icons.favorite_border,
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
